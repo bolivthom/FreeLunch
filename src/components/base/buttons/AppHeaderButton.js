4144c4ad3212af2ab365f6c2b235fefbb92a6e6b
Brittany Thomas <brittanythomas@Brittanys-MacBook-Pro.local>
2022-06-22 15:38:03 -0500

    app header made

--- src/components/base/buttons/AppHeaderButton.js
+++ src/components/base/buttons/AppHeaderButton.js
@@ -1,9 +1,10 @@
 import * as React from "react";
 
-function AppHeaderButton({ Title, BackgroundColor }) {
+function AppHeaderButton({ Title, BackgroundColor, Icon }) {
     return (
         <div className="header-content">
             <button id="app-header-button" style={{backgroundColor: BackgroundColor}}>
+                { Icon }
                 <p className="button-title">{ Title }</p>
             </button>
         </div>
--- src/components/sections/AppHeader.js
+++ src/components/sections/AppHeader.js
@@ -6,19 +6,46 @@ import UsernameButton from "../base/buttons/UsernameButton"
 import AppHeaderButton from "../base/buttons/AppHeaderButton";
 
 //Icons
-//import { ReactComponent as AllCardsIcon } from "../../assets/allcards.svg";
-//import { ReactComponent as DecksIcon } from "../../assets/decks.svg";
+import { ReactComponent as AllCardsIcon } from "../../assets/allcards.svg";
+import { ReactComponent as DecksIcon } from "../../assets/decks.svg";
+
+const styles = {
+    appHeader: {
+        paddingRight: '10%',
+        paddingLeft: '10%',
+        paddingTop: '5%'
+    },
+    leftMenuItems: {
+        display: 'flex',
+        width: '100%',
+        flexDirection: 'row'
+    },
+    headerContent: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    }
+}
 
 function AppHeader() {
-
     return (
-        <div className="header-content">
-            <div className="header-button-group">
-                <AppHeaderButton Title={"All Cards"} BackgroundColor={"#FFFFFF"}/>
-                <AppHeaderButton Title={"Decks"} BackgroundColor={"#E4E4E4"}/>
-                <h6 id="header-title">SW-API Deck Builder</h6>
-                <UsernameButton Title={"Bavin Edwards"} />
+        <div style={styles.appHeader}>
+            <div style={styles.headerContent} className="header-content">
+                <div>
+                    <div style={styles.leftMenuItems}>
+                        <AppHeaderButton Title={"All Cards"} BackgroundColor={"#FFFFFF"} Icon={<AllCardsIcon/>}/>
+                        <AppHeaderButton Title={"Decks"} BackgroundColor={"#E4E4E4"} Icon={<DecksIcon/>}/>
+                    </div>
+                </div>
+                <div>
+                    <h6 id="header-title">SW-API Deck Builder</h6>
+                </div>
+                <div>
+                    <div><UsernameButton Title={"Bavin Edwards"} /></div>
+                </div>
             </div>
+            <hr id="nav-hr"></hr>
         </div>
     );
 }
