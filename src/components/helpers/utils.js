dfaaa10b78f2aeabc1336ba00c33da6ea44d02c3
sirfitz <sirromariof@gmail.com>
2022-07-20 17:41:22 -0600

    ensure message shows when no cards in deck

--- src/components/helpers/utils.js
+++ src/components/helpers/utils.js
@@ -1,3 +1,3 @@
 export const filterByName = (list, searchTerm) => {
-    return list.filter((item) => item.name.toLocaleLowerCase().includes((searchTerm || '').toLocaleLowerCase()))
+    return (list || []).filter((item) => item.name.toLocaleLowerCase().includes((searchTerm || '').toLocaleLowerCase()))
 }
