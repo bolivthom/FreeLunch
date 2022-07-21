b47142853cc65b05fb0fa6088b96104408e34313
sirfitz <sirromariof@gmail.com>
2022-07-20 19:04:00 -0600

    subsitituted sortigng buttons

--- src/components/sections/AppCardListControls.js
+++ src/components/sections/AppCardListControls.js
@@ -19,13 +19,33 @@ const styles = {
 function AppCardControls() {
 
     const { sortBy, setSortBy } = React.useContext(GlobalContext)
-
+    
     return (
         <div className="sub-nav">
             <Searchbar style={styles.cardControls}/>
-            <AppSecondaryButton style={styles.cardControls} Title={"A-Z"} BackgroundColor={"#FFFFFF"} onClick={() => setSortBy('name_asc')} />
-            <AppPrimaryButton style={styles.cardControls} Title={"Youngest"} BackgroundColor={"#E4E4E4"} onClick={() => setSortBy('age_asc')} />
-            <AppPrimaryButton Title={"Eldest"} BackgroundColor={"#E4E4E4"} onClick={() => setSortBy('age_desc')} />
+            {
+                sortBy === 'name_asc' ? (
+                    <AppPrimaryButton style={styles.cardControls} Title={"A-Z"} BackgroundColor={"#FFFFFF"} onClick={() => setSortBy('name_asc')} />
+                ) : (
+                    <AppSecondaryButton style={styles.cardControls} Title={"A-Z"} BackgroundColor={"#E4E4E4"} onClick={() => setSortBy('name_asc')} />
+                )
+            }
+            {
+                sortBy === 'age_asc' ? (
+                    <AppPrimaryButton style={styles.cardControls} Title={"Youngest"}  BackgroundColor={"#FFFFFF"} onClick={() => setSortBy('age_asc')} />
+                ) : (
+                    <AppSecondaryButton style={styles.cardControls} Title={"Youngest"} BackgroundColor={"#E4E4E4"} onClick={() => setSortBy('age_asc')} />
+                )
+            }
+            {
+                sortBy === 'age_desc' ? (
+                    <AppPrimaryButton  style={styles.cardControls} Title={"Eldest"} BackgroundColor={"#FFFFFF"} onClick={() => setSortBy('age_desc')} />
+                ) : (
+                    <AppSecondaryButton  style={styles.cardControls} Title={"Eldest"} BackgroundColor={"#E4E4E4"} onClick={() => setSortBy('age_desc')} />
+                )
+            }
+            
+           
         </div>
 
     );
